eed867db699f1a06ebc8a6180c156bc62d72f1d6
Savita Bevanur <savita_bevanur@persistent.co.in>
2019-08-26 16:03:19 +0530

    Added new line

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
 	public static void main(String[] args){
 		//print Hello World in the terminal window
 		System.out.println("Hello World");
+		System.out.println(" New line added for testing purpose");
 	}
 }
